415fe19a7ab0ca4708c70ae13f972ea1b4b5419d
mohammed <mohammed.abdameeralhalwachi@gmail.com>
2020-04-12 02:09:49 +0300

    make the game the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,120 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        rock-paper-scissors
-      </h1>
-      <h2 class="subtitle">
-        rock paper scissors game
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div id="app" class="flex flex-col w-screen h-screen p-8">
+        <div class="container mx-auto">
+            <top-bar :score="score"></top-bar>
+        </div>
+        
+        <div class="main flex-grow flex justify-center items-center">
+            <transition name="fade" mode="out-in">
+                <Phase1 v-if="phase === 1" @itemClicked="itemClicked" key="phase1"></Phase1>
+                <Phase2 @resetGame="reset" @opponentChosen="setOpponentSelectedItem" v-else="phase === 2" :chosen="selectedItem" key="phase2"></Phase2>
+            </transition>
+        </div>
+        
+        <div @click="showRules" class="rulesButton text-center md:text-right">
+            <button class="button py-3 px-12 border-2 border-gray-500 text-white uppercase tracking-widest rounded-lg">Rules</button>
+        </div>
+
+        <transition name="fade">
+            <rules v-show="isRulesOn" @close="closeRules"></rules>
+        </transition>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+    import TopBar from "../components/TopBar";
+    import Rules from "../components/Rules";
+    import Phase1 from "../components/Phase1";
+    import Phase2 from "../components/Phase2";
 
-export default {
-  components: {
-    Logo
-  }
-}
+    export default {
+        name: "game.vue",
+        components: {Rules, TopBar, Phase1, Phase2},
+        data() {
+            return this.initValues();
+        },
+        methods: {
+            initValues() {
+                let cookieScore = this.$cookies.get('score') ?? 0;
+
+                return {
+                    score: cookieScore,
+                    isRulesOn: false,
+                    selectedItem: null,
+                    opponentSelectedItem: null,
+                    phase: 1,
+                };
+            },
+            showRules() {
+                this.isRulesOn = true;
+            },
+            closeRules() {
+                this.isRulesOn = false;
+            },
+            itemClicked(type) {
+                if (this.selectedItem === null) {
+                    this.selectedItem = type;
+                    this.phase = 2;
+                }
+            },
+            setOpponentSelectedItem(newOpponentSelectedItem) {
+                this.opponentSelectedItem = newOpponentSelectedItem;
+            },
+            endTurn() {
+                console.log(this.winStatus );
+
+                if(this.winStatus === 'win')
+                    this.score++;
+                else if(this.winStatus === 'lose' && this.score > 0)
+                    this.score--;
+            },
+            reset() {
+                Object.assign(this.$data, this.initValues());
+            }
+        },
+        computed: {
+            winStatus() {
+                if(this.selectedItem === this.opponentSelectedItem)
+                    return 'tie';
+                else if(this.selectedItem === 'rock' && this.opponentSelectedItem === 'scissors')
+                    return 'win';
+                else if(this.selectedItem === 'scissors' && this.opponentSelectedItem === 'paper')
+                    return 'win';
+                else if (this.selectedItem === 'paper' && this.opponentSelectedItem === 'rock')
+                    return 'win';
+                else
+                    return 'lose';
+            },
+        },
+        watch: {
+            opponentSelectedItem(val) {
+                if (this.selectedItem !== null && this.opponentSelectedItem !== null) {
+                    this.endTurn();
+                }
+            },
+            score(val) {
+                this.$cookies.set('score', val, Infinity);
+            }
+        }
+    }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped>
+    #app{
+        background: radial-gradient(circle, rgba(31,54,86,1) 0%, rgba(20,23,58,1) 100%);
+    }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+    /*.phase-fade-active, .phase-fade-leave-active {*/
+    /*    transition: opacity .2s;*/
+    /*}*/
+    /*.phase-fade-enter, .phase-fade-leave-to !* .fade-leave-active below version 2.1.8 *! {*/
+    /*    opacity: 0;*/
+    /*}*/
+    
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .2s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
 </style>
